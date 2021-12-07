f49e6216c52e28400114f0eb99667c08d47a97b6
Francesco Varani <francovaro@gmail.com>
2021-12-07 09:49:51 +0100

    filter script

---
+++ filtro_passa_alto.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+c = 10e-6
+r = 10e3
+
+freq = np.arange(0, 1000, 0.01)
+yp = ((r*c*freq*2*3.14)/(1+c*r*freq*2*3.14))
+
+plt.plot(freq, yp, "g")
+
+#plt.yscale('log')
+plt.xscale('log')
+
+plt.grid(True)
+plt.show()
